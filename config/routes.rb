aac6b7460065b2afd34f090630f34f31817c6835
Mauricio Moraga <mmoraga.m3@gmail.com>
2024-04-09 13:24:05 -0400

    configuring-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :records
-  resources :artists
+
+  namespace :api do
+    namespace :v1 do
+      resources :records
+      resources :artists
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,5 +13,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "home#index"
 end
